c5d55156876c908083255c9cb41a80b9871340cf
CynthiaKamau <cynthiakamau54@gmail.com>
2026-04-14 12:18:46 +0300

    Add rtc data extract query

---
+++ DWAPI_New_Extracts/RTCRiskScore.sql
@@ -0,0 +1,16 @@
+SELECT r.patient_id            as PatientPK,
+       i.siteCode              as SiteCode,
+       i.FacilityName          as FacilityName,
+       'KenyaEMR'              as Emr,
+       ''                      as EMRVersion,
+       'Kenya HMIS RTC'        as Project,
+       r.source_system_uuid    as SourceSysUUID,
+       r.rtc_risk_score        as RtcRiskScore,
+       r.rtc_category          as RtcRiskCategory,
+       r.rtc_evaluation_date   as RtcEvaluationDate,
+       r.date_created          as DateCreated,
+       r.date_changed          as DateLastModified,
+       r.voided                as voided
+FROM openmrs.kenyaemr_ml_patient_risk_score r
+         inner join dwapi_etl.etl_patient_demographics d on d.patient_id = r.patient_id
+         join kenyaemr_etl.etl_default_facility_info i;

8d96bde06600c2e68162aa0fba988f0429e4fe47
CynthiaKamau <cynthiakamau54@gmail.com>
2026-05-20 11:25:01 +0300

    Add ml risk score model version

--- DWAPI_New_Extracts/IITRiskScore.sql
+++ DWAPI_New_Extracts/IITRiskScore.sql
@@ -13,7 +13,8 @@ SELECT r.patient_id            as PatientPK,
        e.next_appointment_date as NextAppointmentDate,
        r.date_created          as DateCreated,
        r.date_changed          as DateLastModified,
-       r.voided                as voided
+       r.voided                as voided,
+       r.model_version         as ModelVersion
 FROM openmrs.kenyaemr_ml_patient_risk_score r
          inner join dwapi_etl.etl_patient_demographics d on d.patient_id = r.patient_id
          inner join(select f.patient_id,
